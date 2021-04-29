7c0b2075468e830de3fdfe9e3d31499300ed2840
Cool77Catt <chris77carl@gmail.com>
2021-04-28 20:45:34 -0400

    first commit

---
+++ components/WorkoutMonitor/WorkoutMonitor.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default function WorkoutMonitor(): JSX.Element {
+  return (
+    <View >
+      <Text>Workout Monitor</Text>
+    </View>
+  );
+}
